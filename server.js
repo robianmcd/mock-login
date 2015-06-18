6287f9da9970fde8076c3a9132c4008125ad6195
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2015-06-17 22:44:16 -0400

    added some more CORS black magic

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ var chance = new Chance();
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
