1150659767b3c4795edda52797994ff8728b334f
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2015-06-18 12:41:43 -0400

    cors...

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var chance = new Chance();
 //Enable cors
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
